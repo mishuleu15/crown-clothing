6d81da96a5cee85fedb6f338fb3483161c0a592c
mihaiCatalin15 <atudosieacatalin@gmail.com>
2020-06-12 21:55:28 +0300

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data(),
             },
-          });
+            
+          },() => console.log(this.state));
+          
         });
       } else {
         this.setState({ currentUser: userAuth });
